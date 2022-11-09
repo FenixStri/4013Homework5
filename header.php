f8b40f9ecc65b03236bee8d2719e5ef9d5941d4e
Fenix <89755740+FenixStri@users.noreply.github.com>
2022-11-09 11:08:12 -0600

    Create header.php

---
+++ header.php
@@ -0,0 +1,50 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    
+    
+    
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+     <nav class="navbar navbar-expand-lg bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="courses.php">Courses Page</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="instructors.php">Instructors Page</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="instructor-cards.php">Instructor Cards Page</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="sections.php">Sections Page</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="dogs.php">Dogs Page</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="cats.php">Cats Page</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled">Disabled</a>
+        </li>
+      </ul>
+      <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+  </head>
+  
+  <body>
