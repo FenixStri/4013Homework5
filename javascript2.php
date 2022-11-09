9b99e705b1d3906b86c5d6efe927013220b1b33f
Fenix <89755740+FenixStri@users.noreply.github.com>
2022-11-09 11:59:56 -0600

    Create javascript2.php

---
+++ javascript2.php
@@ -0,0 +1,17 @@
+
+<script>
+function myFunction() {
+  document.getElementById("demo").innerHTML = "The sauce is saucey";
+}
+</script>
+
+
+<p>When you click "Try it", a function will be called.</p>
+<p>The function will display a message.</p>
+
+<button onclick="myFunction()">Try it</button>
+
+<p id="demo"></p>
+
+</body>
+</html>
